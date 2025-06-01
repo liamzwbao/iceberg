6d185c01562a7a807ecd158b0714f0cc46b43719
Liam Bao <liam.zw.bao@gmail.com>
2025-06-01 11:42:48 -0400

    Deprecation instead of removing

--- flink/v1.20/flink/src/main/java/org/apache/iceberg/flink/source/IcebergSource.java
+++ flink/v1.20/flink/src/main/java/org/apache/iceberg/flink/source/IcebergSource.java
@@ -41,6 +41,7 @@ import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.catalog.ResolvedSchema;
 import org.apache.flink.table.data.RowData;
 import org.apache.iceberg.BaseMetadataTable;
@@ -291,6 +292,7 @@ public class IcebergSource<T> implements Source<T, IcebergSourceSplit, IcebergEn
     private RowDataConverter<T> converter;
     private ReadableConfig flinkConfig = new Configuration();
     private final ScanContext.Builder contextBuilder = ScanContext.builder();
+    private TableSchema projectedTableSchema;
     private ResolvedSchema projectedFlinkSchema;
     private Boolean exposeLocality;
 
@@ -458,6 +460,16 @@ public class IcebergSource<T> implements Source<T, IcebergSourceSplit, IcebergEn
       return this;
     }
 
+    /**
+     * @deprecated since 1.10.0, will be removed in 2.0.0. Use {@link #project(ResolvedSchema)}
+     *     instead.
+     */
+    @Deprecated
+    public Builder<T> project(TableSchema newProjectedFlinkSchema) {
+      this.projectedTableSchema = newProjectedFlinkSchema;
+      return this;
+    }
+
     public Builder<T> project(ResolvedSchema newProjectedFlinkSchema) {
       this.projectedFlinkSchema = newProjectedFlinkSchema;
       return this;
@@ -563,6 +575,8 @@ public class IcebergSource<T> implements Source<T, IcebergSourceSplit, IcebergEn
       Schema icebergSchema = table.schema();
       if (projectedFlinkSchema != null) {
         contextBuilder.project(FlinkSchemaUtil.convert(icebergSchema, projectedFlinkSchema));
+      } else if (projectedTableSchema != null) {
+        contextBuilder.project(FlinkSchemaUtil.convert(icebergSchema, projectedTableSchema));
       }
 
       SerializableRecordEmitter<T> emitter = SerializableRecordEmitter.defaultEmitter();
